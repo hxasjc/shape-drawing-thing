91c4f8955c7a695195bbcf32995826835119284b
hxasjc <karlvonweyhe@gmail.com>
2022-09-16 15:29:28 -0400

    comments

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,24 @@ import java.util.NoSuchElementException;
 import java.util.Stack;
 
 public class Main extends Window {
-    private static final double POINT_SIZE = 5;
+    private static final double POINT_SIZE = 5; //size of pointss
 
-    PointShape pointShape = PointShape.SQUARE;
-    Text shapeLabel;
-    Text lineLabel;
-    Stack<Renderable> points = new Stack<>();
-    Stack<Line> lines = new Stack<>();
-    Stack<LastType> lastType = new Stack<>();
-    boolean lineModeEnabled = false;
+    PointShape pointShape = PointShape.SQUARE; //current shape of points
+    Text shapeLabel; //label to show point shape
+    Text lineLabel; //label to show if lines are enabled
+    Stack<Renderable> points = new Stack<>(); //stack of points
+    Stack<Line> lines = new Stack<>(); //stack of lines
+    Stack<LastType> lastType = new Stack<>(); //stack to store order of object types created
+    boolean lineModeEnabled = false; //are lines enabled
 
     public static void main(String[] args) {
-        Window window = new Window();
+        Window window = new Window(); //standard setup
         window.open(800, 800, "Shape Drawer");
     }
 
     @Override
     public void start() {
-        shapeLabel = new TextHelper(screen, "Current Shape: " + pointShape.toString(), new Location(10, 10)).create();
+        shapeLabel = new TextHelper(screen, "Current Shape: " + pointShape.toString(), new Location(10, 10)).create(); //create info labels
         lineLabel = new TextHelper(screen, "Line Mode: " + lineModeEnabled, new Location(10, 25)).create();
     }
 
@@ -30,20 +30,20 @@ public class Main extends Window {
     public void mouseDown(Location location, int button) {
         print(location, button); //debug
         switch (button) {
-            case 1 -> {
+            case 1 -> { //left click (create new point)
                 Renderable point = null;
                 Renderable lastPoint = null;
-                try {
+                try { //get last point to draw line
                     lastPoint = points.lastElement();
-                } catch (NoSuchElementException e) {
+                } catch (NoSuchElementException e) { //for when stack is empty
                     //ignore
                 }
-                switch (pointShape) {
+                switch (pointShape) { //draw shape of corresponding type
                     case SQUARE -> {
-                        point = new Rectangle(screen, location, POINT_SIZE, POINT_SIZE);
-                        point.center(location);
-                        points.add(point);
-                        lastType.add(LastType.POINT);
+                        point = new Rectangle(screen, location, POINT_SIZE, POINT_SIZE); //create shape
+                        point.center(location); //center shape on mouse position
+                        points.add(point); //add to point stack
+                        lastType.add(LastType.POINT); //add point to lastType stack
                     }
                     case CIRCLE -> {
                         point = new Oval(screen, location, POINT_SIZE, POINT_SIZE);
@@ -58,24 +58,24 @@ public class Main extends Window {
                         lastType.add(LastType.POINT);
                     }
                 }
-                if (lineModeEnabled) {
-                    Line line = new Line(screen, point.center(), lastPoint.center());
-                    lines.add(line);
-                    lastType.add(LastType.LINE);
+                if (lineModeEnabled) { //draw line
+                    Line line = new Line(screen, point.center(), lastPoint.center()); //create line
+                    lines.add(line); //add to stack
+                    lastType.add(LastType.LINE); //add line to lastType stack
                 }
             }
-            case 2 -> {
-                pointShape = nextEnum(PointShape.class, pointShape);
+            case 2 -> { //middle click (cycle through point shapes)
+                pointShape = nextEnum(PointShape.class, pointShape); //get next enum
                 print(pointShape); //debug
                 updateShapeLabel();
             }
-            case 3 -> {
+            case 3 -> { //right click (remove last item)
                 try {
-                    switch (lastType.pop()) {
-                        case POINT -> points.pop().remove();
+                    switch (lastType.pop()) { //get last item created
+                        case POINT -> points.pop().remove(); //delete it from the correct stack and remove it from the screen
                         case LINE -> lines.pop().remove();
                     }
-                } catch (EmptyStackException e) {
+                } catch (EmptyStackException e) { //if a stack is empty
                     //ignore
                 }
             }
@@ -85,33 +85,33 @@ public class Main extends Window {
     @Override
     public void keyDown(Key key) {
         switch (key.character()) {
-            case "E" -> {
-                points.forEach((p) -> p.remove());
+            case "E" -> { //delete all points
+                points.forEach((p) -> p.remove()); //remove all points from screen
                 points.empty();
-                lastType.removeIf((lt) -> lt.equals(LastType.POINT));
-                System.out.println(lastType);
+                lastType.removeIf((lt) -> lt.equals(LastType.POINT)); //remove all points from lastType stack
+                System.out.println(lastType); //debug
             }
-            case "L" -> lineModeEnabled = !lineModeEnabled;
-            case "C" -> {
-                lines.forEach((l) -> l.remove());
+            case "L" -> lineModeEnabled = !lineModeEnabled; //toggle line mode
+            case "C" -> { //delete all lines
+                lines.forEach((l) -> l.remove()); //remove all lines from screen
                 lines.empty();
-                lastType.removeIf((lt) -> lt.equals(LastType.LINE));
+                lastType.removeIf((lt) -> lt.equals(LastType.LINE)); //remove all lines from lastType stack
             }
-            case "1" -> System.out.println(points);
-            case "2" -> System.out.println(lines);
-            case "3" -> System.out.println(lastType);
+            case "1" -> System.out.println(points); //debug (non-functional)
+            case "2" -> System.out.println(lines); //debug (non-functional)
+            case "3" -> System.out.println(lastType); //debug (non-functional)
         }
     }
 
-    public void updateShapeLabel() {
+    public void updateShapeLabel() { //updates shape label text
         shapeLabel.text("Current Shape: " + pointShape.toString());
     }
 
-    public static <T extends Enum<T>> T nextEnum(Class<T> clazz, T currentEnum) {
-        T[] enumItems = clazz.getEnumConstants();
+    public static <T extends Enum<T>> T nextEnum(Class<T> clazz, T currentEnum) { //generic fun
+        T[] enumItems = clazz.getEnumConstants(); //get enum items of given class (because the values() method is not in the Enum class)
         int currentIndex = -1;
 
-        for (int i = 0; i < enumItems.length; i++) {
+        for (int i = 0; i < enumItems.length; i++) { //find index of current enum item in array
             if (enumItems[i].equals(currentEnum)) {
                 currentIndex = i;
             }
@@ -119,18 +119,18 @@ public class Main extends Window {
 
         int nextIndex = currentIndex + 1;
 
-        if (nextIndex >= enumItems.length) {
+        if (nextIndex >= enumItems.length) { //prevent overflow, cycle back to front of array
             nextIndex -= enumItems.length;
         }
 
         return enumItems[nextIndex];
     }
 
-    public enum PointShape {
+    public enum PointShape { //defines the point shapes
         SQUARE, CIRCLE, TRIANGLE
     }
 
-    public enum LastType {
+    public enum LastType { //defines lastType
         POINT, LINE
     }
 }
--- src/TextHelper.java
+++ src/TextHelper.java
@@ -3,8 +3,11 @@ import javadraw.Location;
 import javadraw.Screen;
 import javadraw.Text;
 
+/**
+ * Easier construction of Text objects, makes it easy to change lesser used params like bold/italic without using the full constructor
+ */
 public class TextHelper {
-    private final Screen screen;
+    private final Screen screen; //param variables
     private final Object text;
     private final Location location;
     private Color color;
@@ -19,6 +22,12 @@ public class TextHelper {
     private double rotation;
     private boolean visible;
 
+    /**
+     * Accepts required params and sets other params to default values
+     * @param screen screen to display on
+     * @param text text to display
+     * @param location location to place the object at
+     */
     public TextHelper(Screen screen, Object text, Location location) {
         this.screen = screen;
         this.text = text;
@@ -36,61 +45,124 @@ public class TextHelper {
         this.visible = true;
     }
 
+    /**
+     * sets color param
+     * @param color text color
+     * @return current object
+     */
     public TextHelper color(Color color) {
         this.color = color;
         return this;
     }
 
+    /**
+     * sets the border color param
+     * @param border border color
+     * @return current object
+     */
     public TextHelper border(Color border) {
         this.border = border;
         return this;
     }
 
+    /**
+     * sets the font param
+     * <br/>
+     * uses java.awt font names
+     * @param font font to use
+     * @return current object
+     */
     public TextHelper font(String font) {
         this.font = font;
         return this;
     }
 
+    /**
+     * sets the text size param
+     * @param size text size
+     * @return current object
+     */
     public TextHelper size(int size) {
         this.size = size;
         return this;
     }
 
+    /**
+     * sets the text alignment param
+     * <br/>
+     * LEFT, CENTER, RIGHT
+     * @param align text alignment
+     * @return current object
+     */
     public TextHelper align(String align) {
         this.align = align;
         return this;
     }
 
+    /**
+     * sets the bold text param
+     * @param bold if text is bold
+     * @return current object
+     */
     public TextHelper bold(boolean bold) {
         this.bold = bold;
         return this;
     }
 
+    /**
+     * sets the italic text param
+     * @param italic if text is italic
+     * @return current object
+     */
     public TextHelper italic(boolean italic) {
         this.italic = italic;
         return this;
     }
 
+    /**
+     * sets the text underline param
+     * @param underline if text is underlined
+     * @return current object
+     */
     public TextHelper underline(boolean underline) {
         this.underline = underline;
         return this;
     }
 
+    /**
+     * sets the text strikethrough param
+     * @param strikethrough if text is strikethrough
+     * @return current object
+     */
     public TextHelper strikethrough(boolean strikethrough) {
         this.strikethrough = strikethrough;
         return this;
     }
 
+    /**
+     * sets the object rotation param
+     * @param rotation object rotation
+     * @return current object
+     */
     public TextHelper rotation(double rotation) {
         this.rotation = rotation;
         return this;
     }
 
+    /**
+     * sets object visibility param
+     * @param visible is object visible
+     * @return current object
+     */
     public TextHelper visible(boolean visible) {
         this.visible = visible;
         return this;
     }
 
+    /**
+     * takes all given params and returns a javadraw.Text object
+     * @return {@code javadraw.Text} object
+     */
     public Text create() {
         return new Text(screen, text, location, color, border, font, size, align, bold, italic, underline, strikethrough, rotation, visible);
     }
